94bd871e8ed5d1f9fd1454c7d8031a8821c00e71
FotisBampaniotis <90285044+FotisBaba@users.noreply.github.com>
2021-09-08 02:38:01 +0300

    Add files via upload

---
+++ Code/AreaData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaData : MonoBehaviour
+{
+    public int buyingPrice;
+    public GameObject[] buildings;
+    public GameObject[] entrances;
+    public GameObject[] buildingPrice;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
