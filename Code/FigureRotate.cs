a9631e6620561868d3ee9d1c361fa7790ab41387
FotisBampaniotis <90285044+FotisBaba@users.noreply.github.com>
2021-09-08 02:40:26 +0300

    Add files via upload

---
+++ Code/FigureRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FigureRotate : MonoBehaviour
+{
+    public GameObject figure;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        figure.transform.Rotate(0, 0.008f, 0, Space.Self);
+    }
+}
