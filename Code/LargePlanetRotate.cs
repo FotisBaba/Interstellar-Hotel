1e2f960079ad12aca899c5bbdd5eae5b5707dfd2
FotisBampaniotis <90285044+FotisBaba@users.noreply.github.com>
2021-09-08 02:41:34 +0300

    Add files via upload

---
+++ Code/LargePlanetRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargePlanetRotate : MonoBehaviour
+{
+
+    public GameObject LargePlanet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LargePlanet.transform.Rotate(0, .008f, 0, Space.Self);
+    }
+}
