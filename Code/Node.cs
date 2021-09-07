f76979e9cde9adff936a1a8506cfd5d13ac294b4
FotisBampaniotis <90285044+FotisBaba@users.noreply.github.com>
2021-09-08 02:42:07 +0300

    Add files via upload

---
+++ Code/Node.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gameNameSpace {
+    public class Node : MonoBehaviour
+    {
+        public GameObject previousNode;
+
+        public GameObject node;
+
+        public GameObject nextNode;
+
+        public GameObject innerEntranceObject;
+
+        public GameObject outerEntranceObject;
+
+        [SerializeField] private bool hasEntrance = false;
+
+
+        public GameObject[] positions;
+
+        private int playersOnTile = 0;
+
+        [SerializeField] public GameObject innerAreaObject;
+        [SerializeField] public GameObject outerAreaObject;
+
+        [SerializeField] private NodeType type;
+        [SerializeField] private bool innerEntrance;
+        [SerializeField] private bool outerEnrance;
+        [SerializeField] private int ID;
+
+        public int PlayersOnTile { get => playersOnTile; set => playersOnTile = value; }
+        public int ID1 { get => ID; set => ID = value; }
+        public NodeType Type { get => type; set => type = value; }
+        public bool HasEntrance { get => hasEntrance; set => hasEntrance = value; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Debug.Log(type);
+            // Debug.Log(innerEntrance);
+            //Debug.Log(ID);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public enum NodeType
+        {
+            BuyNode,
+            BuildNode,
+            FreeBuildNode,
+            FreeEntranceNode
+        }
+
+        void OnMouseDown()
+        {
+            Debug.Log("MouseDOOOOOOOOOWN");
+            if (GameObject.Find("System").GetComponent<GameFlow>().waitingForTile)
+            {
+                Debug.Log("wait for tile true");
+                GameObject.Find("System").GetComponent<GameFlow>().EntranceTileToBuild = this.node;
+                GameObject.Find("System").GetComponent<GameFlow>().waitingForTile = false;
+                GameObject.Find("System").GetComponent<GameFlow>().tileSelected = true;
+            }
+        }
+    }
+}
---
+++ Code/Player.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gameNameSpace
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerInfo pdata;
+
+
+        public PlayerInfo Pdata { get => pdata; set => pdata = value; }
+
+
+
+        public void movePlayer(int dist)
+        {
+            
+            
+            for (int m = 0; m < dist; m++)
+            {
+
+                StartCoroutine(Run(5f, 3f));
+            }
+            
+            
+        }
+
+        private IEnumerator Run(float duration, float waitTime)
+        {
+            //int index = 0;
+            
+            float time = 0;
+            WaitForSeconds wait = new WaitForSeconds(waitTime);
+            Vector3 startPosition = this.pdata.Figure.transform.position;
+            Vector3 endPosition = getPosOnTile(this.pdata.CurrentPosTile.GetComponent<Node>().nextNode).position;
+
+            for (time = 0; time < duration; time += Time.deltaTime){
+
+                    this.pdata.Figure.transform.position = Vector3.Lerp(startPosition, endPosition, time);
+
+            }
+            //yield return new WaitUntil(() => startPosition == endPosition);
+            this.pdata.Figure.transform.rotation = this.pdata.CurrentPosTile.GetComponent<Node>().nextNode.transform.rotation;
+            this.pdata.Figure.transform.rotation *= Quaternion.Euler(0, -90, 0);
+            this.pdata.PreviousPosTile = this.pdata.CurrentPosTile;
+            this.pdata.CurrentPosTile = this.pdata.CurrentPosTile.GetComponent<Node>().nextNode;
+            this.pdata.Figure.transform.position = endPosition;
+            checkForIncome();
+            checkForEntrance();
+            yield return wait;
+            
+ 
+        }
+
+
+        private Transform getPosOnTile(GameObject destTile)
+        {
+            if(destTile.GetComponent<Node>().PlayersOnTile == 0)
+            {
+                return destTile.GetComponent<Node>().positions[0].transform;
+            }
+            else if (destTile.GetComponent<Node>().PlayersOnTile == 1)
+            {
+                return destTile.GetComponent<Node>().positions[1].transform;
+            }
+            else if(destTile.GetComponent<Node>().PlayersOnTile == 2)
+            {
+                return destTile.GetComponent<Node>().positions[2].transform;
+            }
+            else if (destTile.GetComponent<Node>().PlayersOnTile == 3)
+            {
+                return destTile.GetComponent<Node>().positions[3].transform;
+            }
+            return null;
+        }
+
+        private void checkForIncome()
+        {
+            if(this.pdata.PreviousPosTile.GetComponent<Node>().ID1 == 9 && this.pdata.CurrentPosTile.GetComponent<Node>().ID1 == 10)
+            {
+                this.pdata.Cash += 3000;
+                GameObject.Find("UserInterface").GetComponent<InterfaceDisplay>().updatePlayerStats();
+            }
+        }
+
+        private void checkForEntrance()
+        {
+            Debug.Log("CHECK ENTRANCE");
+            if (this.pdata.PreviousPosTile.GetComponent<Node>().ID1 == 17 && this.pdata.CurrentPosTile.GetComponent<Node>().ID1 == 18)
+            {
+                GameObject.Find("System").GetComponent<GameFlow>().State = GameFlow.turnState.BuyEntrance;
+                GameObject.Find("System").GetComponent<GameFlow>().ActionStarted = false;
+                GameObject.Find("System").GetComponent<GameFlow>().BuyEntranceCounter = 0;
+            }
+        }
+
+    }
+
+
+    
+}
